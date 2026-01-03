b01ea3ea9d8419fe7ece12bd4fcace4eb94a0d8e
franců <francupatrik@seznam.cz>
2026-01-03 11:54:36 +0100

    6. commit - registrace name, lastname, email, username

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
