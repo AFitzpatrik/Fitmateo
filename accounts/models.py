4df99eceb5da6e78d95c84a58d3bd228133efb5a
franců <francupatrik@seznam.cz>
2026-01-03 12:00:38 +0100

    ACCOUNTS - Návrh profilu pro usera

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,25 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+'''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+    age = models.PositiveIntegerField()
+    city = models.ForeignKey(
+        "City",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    sports = models.ManyToManyField(
+        "Sport",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"Profile of {self.user.email}"
+        '''
+
+#NÁVRH DODĚLAT POZDĚJI
