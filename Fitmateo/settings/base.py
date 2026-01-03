a8cd8123b2f0e630c5dbfeaaa53b37121c6b0fcc
franců <francupatrik@seznam.cz>
2026-01-03 11:46:32 +0100

    4. commit - Nastavení sqlite

--- Fitmateo/settings/base.py
+++ Fitmateo/settings/base.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts'
-    'viewer'
+    'accounts',
+    'viewer',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
